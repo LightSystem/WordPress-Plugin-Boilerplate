17dfca229fa99b8ca814ec854e6038d51174a08e
João Horta Alves <joao.alves@log.pt>
2014-03-12 00:00:16 +0000

    changes to js

--- rss-sync/admin/assets/js/admin.js
+++ rss-sync/admin/assets/js/admin.js
@@ -1,5 +1,9 @@
 jQuery(document).ready( function () {
 
+    if(jQuery('#image_storage_options').value == "hotlinking"){
+        jQuery('#thumb_options_set').hide();
+    }
+
     jQuery('#image_storage_options').on('change', function(){
 
         if(this.value == 'local_storage'){
